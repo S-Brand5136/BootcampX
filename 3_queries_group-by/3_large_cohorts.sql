99f18b78846c5f0302dd6c8207c677e536d5a8ab
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-24 17:14:29 -0600

    add query for cohorts over 18 students

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name as cohort_name, COUNT(students.name) as student_count
+  FROM cohorts
+  JOIN students ON (cohorts.id = students.cohort_id)
+  GROUP BY cohorts.name
+  HAVING COUNT(students.name) >= 18
+  ORDER BY student_count; 

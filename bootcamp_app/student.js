8d1e478242f0bae5bb050860e9185b28e8ae5532
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-26 18:34:34 -0600

    update student.js to use parameterized queries

--- bootcamp_app/student.js
+++ bootcamp_app/student.js
@@ -2,7 +2,9 @@ const { Pool } = require('pg');
 const userInput = process.argv.slice(2);
 
 const cohort = userInput[0].toUpperCase();
-const maxResults = userInput[1];
+const maxResults = userInput[1] || 5;
+
+const values = [`%${cohort}%`, maxResults];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -15,9 +17,9 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${maxResults || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);

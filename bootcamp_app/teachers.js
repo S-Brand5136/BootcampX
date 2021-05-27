ca3e8e17106d3dbf33352e8ccdd1f29e6c511698
Brandon Shemilt <Brandon.Shemilt@live.co.uk>
2021-05-26 18:36:46 -0600

    update teachers.js to use parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,7 +1,9 @@
 const { Pool } = require('pg');
 const userInput = process.argv.slice(2);
 
-const cohort = userInput[0];
+const cohort = userInput[0] || 'JUL02';
+
+const values = [`${cohort}`];
 
 const pool = new Pool({
   user: 'vagrant',
@@ -20,9 +22,9 @@ FROM assistance_requests
 JOIN teachers ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name = '${cohort || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teachers.name;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
